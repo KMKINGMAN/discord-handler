47014b36c3bec938a5d7a2ebb50ee99e774212d6
Muhammad Rafat Kurkar <84540534+KMKINGMAN@users.noreply.github.com>
2022-09-15 13:10:23 +0300

    add buttons collection

--- src/lib/utils/collections.ts
+++ src/lib/utils/collections.ts
@@ -1,13 +1,5 @@
-/**
- *     public prefix_commands: Collection<string, Promise<void>>;
-    public slash_commands: Collection<string, Promise<void>>;
-    public user_commands: Collection<string, Promise<void>>;
-    public message_commands: Collection<string, Promise<void>>;
-    public modals: Collection<string, Promise<void>>;
-    public events: Collection<string, Promise<void>>;
- */
 import { Collection } from "discord.js";
-import { CommandFilerType, general, message_command, modal, slachcmd, user_command } from "./../handler/command"
+import { CommandFilerType, general, message_command, modal, slachcmd, user_command, buttons_type } from "./../handler/command"
 export let collection = () => {
     return {
         user_commands: new Collection<string, user_command>(),
@@ -15,6 +7,7 @@ export let collection = () => {
         message_commands: new Collection<string, message_command>(),
         prefix_command: new Collection<string, general>(),
         modals: new Collection<string, modal>(),
-        events: new Collection<string, Promise<void>>()
+        buttons: new Collection<string, buttons_type>(),
+        events: new Collection<string, Promise<void>>(),
     }
 }
