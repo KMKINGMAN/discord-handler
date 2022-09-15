d8a3501f335c427daef3680bf6dcfed40add3de2
Muhammad Rafat Kurkar <84540534+KMKINGMAN@users.noreply.github.com>
2022-09-15 13:12:40 +0300

    test the buttons handler

--- src/commands/test/test-message.ts
+++ src/commands/test/test-message.ts
@@ -1,4 +1,4 @@
-import { ModalBuilder, TextInputBuilder, TextInputStyle, ActionRowBuilder, ModalActionRowComponentBuilder } from "discord.js";
+import { ModalBuilder, TextInputBuilder, TextInputStyle, ActionRowBuilder, ModalActionRowComponentBuilder, ButtonBuilder, ButtonStyle } from "discord.js";
 import { CommandFilerType } from "../../lib/handler/command";
 export let command = {
     message_command: {
@@ -25,17 +25,28 @@ export let command = {
             bot: "Administrator"
         },
         async run(client, interaction) {
-            const modal = new ModalBuilder()
-                .setCustomId('test')
-                .setTitle('KINGMAN TEST');
-            const something = new TextInputBuilder()
-                .setCustomId('hi')
-                .setLabel("Welcome")
-                .setStyle(TextInputStyle.Short);
-            const ActionRow = new ActionRowBuilder<ModalActionRowComponentBuilder>().addComponents(something);
-            modal.setComponents(ActionRow)
-            await interaction.showModal(modal);
-
+            let buttons = new ButtonBuilder();
+            buttons.setCustomId("test")
+            .setStyle(ButtonStyle.Success)
+            .setLabel("Hi");
+            // const modal = new ModalBuilder()
+            //     .setCustomId('test')
+            //     .setTitle('KINGMAN TEST');
+            // const something = new TextInputBuilder()
+            //     .setCustomId('hi')
+            //     .setLabel("Welcome")
+            //     .setStyle(TextInputStyle.Short);
+            // const ActionRow = new ActionRowBuilder<ModalActionRowComponentBuilder>().addComponents(something);
+            // modal.setComponents(ActionRow)
+            // await interaction.showModal(modal);
+            const row = new ActionRowBuilder<ButtonBuilder>()
+			.addComponents(
+                buttons
+			);
+            interaction.reply({
+                content: `**Test**`,
+                components: [row]
+            })
         },
     },
     modal: {
@@ -51,5 +62,13 @@ export let command = {
         async run(client, message, args, manager) {
             message.reply("**test**")
         },
-    }
+    },
+    buttons: [{
+        id: "test",
+        async run(client, interaction) {
+            interaction.reply({
+                content: "Button Work"
+            })
+        },
+    }]
 } as CommandFilerType
