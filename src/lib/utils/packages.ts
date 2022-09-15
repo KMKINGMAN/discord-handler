c2d4d4abc898d81c1d92a4076553b6f6f072e19c
Muhammad Rafat Kurkar <84540534+KMKINGMAN@users.noreply.github.com>
2022-09-15 13:59:49 +0300

    add new packages to KMCodes Client

--- src/lib/utils/packages.ts
+++ src/lib/utils/packages.ts
@@ -1,8 +1,14 @@
 import fs from "fs";
 import efs from "fs-extra";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
+import * as mongo from "mongoose";
+import chalk from "chalk";
+import figlet from "figlet";
 export {
     fs,
     efs,
-    dotenv
+    dotenv,
+    mongo,
+    chalk,
+    figlet
 }
